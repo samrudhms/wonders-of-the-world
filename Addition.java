e5b0355c0e8f9e2b1cdf6f92f9066cea18375c48
samrudhms <samrudh023@gmail.com>
2025-11-09 14:37:49 +0530

    v6

---
+++ Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+    public static void main(String[] args) {
+        int num1 = 25;   // first number
+        int num2 = 75;   // second number
+
+        int sum = num1 + num2; // addition
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
